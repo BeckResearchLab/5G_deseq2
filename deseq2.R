b4ef41a901cd0ccbe62a5ffb1e25c255e491ea01
Dave Beck <dacb@uw.edu>
2020-03-16 12:19:58 -0700

    commit from checkpoint

---
+++ deseq2.R
@@ -0,0 +1,28 @@
+library(DESeq2)
+
+# the count matrix was generated with the following command run from within /work/5GB1c_rnaseq
+# python barrelseq/shim.py extract --config-file barrelseq_config_gb.yaml --output data/extract_RAW_counts.tsv --values raw --format tsv
+# read in the count matrix
+countdata <- read.table("data/extract_RAW_counts.tsv", header=TRUE, row.names=1, sep='\t')
+countdata <- countdata[,0:98]
+countdata <- as.matrix(countdata)
+#head(countdata)
+#names(countdata)
+
+# read in the sample matrix 
+coldata <- read.table("data/5G_exp_metadata.tsv", header=TRUE, sep='\t', comment.char = '', row.names=3)
+coldata <- coldata[,c(1, 2)]
+#rownames(coldata) <- coldata[,1]
+head(as.data.frame(coldata))
+
+colnames(countdata)
+all(colnames(countdata) %in% rownames(coldata))
+all(rownames(coldata) %in% colnames(countdata))
+all(rownames(coldata) == colnames(countdata))
+
+# run the deseq2 estimators
+dds <- DESeqDataSetFromMatrix(countData=countdata, colData=coldata, design=~exp_condition)
+dds
+
+# Run the DESeq pipeline
+dds <- DESeq(dds)
